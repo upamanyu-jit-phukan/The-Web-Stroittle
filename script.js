4efa5a3cd44009993be2e9ca896edc2ff5bce71a
Bhargabcodes <149865916+Bhargabcodes@users.noreply.github.com>
2024-04-05 23:57:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 2,
+    spaceBetween: 25,
+    slidesPerGroup:2,
+    centerSlide:'true',
+    fade:'true',
+    grabCursor:'true',
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets:true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints:{
+        0:{
+            slidesPerView: 1, 
+        },
+        520:{
+            slidesPerView: 2,
+        }
+    }
+  });
+
+  function toggleDetails(id){
+    var details = document.getElementById(id);
+    details.classList.toggle("show");
+  }
+
